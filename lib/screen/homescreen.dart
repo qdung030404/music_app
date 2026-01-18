3797b46af9df2d580f9e5df3060474d7f949c506
qdung030404 <qdung030404@gmail.com>
2026-01-18 23:09:27 +0700

    feat: implement bottom navigation with four main tabs
    - Create HomeScreen with CupertinoTabScaffold
      * Use CupertinoPageScaffold for iOS-style navigation
      * Implement CupertinoTabBar with 4 tabs: Home, Discovery, Account, Setting
      * Configure tab bar background color from theme
    
    - Add placeholder tab screens
      * HomeTabs: main music feed (placeholder)
      * DiscoveryTab: music discovery features (placeholder)
      * UserTab: user profile and library (placeholder)
      * SettingTab: app settings (placeholder)
    
    - Update main.dart to launch HomeScreen
      * Remove debug test code
      * Set HomeScreen as initial route
    
    - Configure Material 3 theme
      * Use ColorScheme.fromSeed with deepPurple
      * Enable useMaterial3 flag
    
    UI structure:
    - Tab icons: home, album, person, settings
    - Navigation: iOS-style Cupertino components
    - Theme: Material 3 with custom color scheme

---
+++ lib/screen/homescreen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:music_app/screen/tab/setting/setting.dart';
+import 'package:music_app/screen/tab/user/user.dart';
+import 'package:music_app/screen/tab/discovery/discovery.dart';
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music App',
+      theme: ThemeData(
+      useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Widget> _tabs = [
+    const HomeTabs(),
+    const DiscoveryTab(),
+    const UserTab(),
+    const SettingTab(),
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Music App'),
+      ),
+        child: CupertinoTabScaffold(
+          tabBar: CupertinoTabBar(
+            backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.album), label: 'Discovery'),
+              BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+              BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+            ],
+          ),
+          tabBuilder: (BuildContext context, int index) {
+            return _tabs[index];
+          },
+        ),
+    );
+  }
+}
+
+class HomeTabs extends StatelessWidget {
+  const HomeTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
