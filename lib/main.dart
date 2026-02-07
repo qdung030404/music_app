825c22b7619df10d5cf602463de09550b3a07a37
qdung030404 <qdung030404@gmail.com>
2026-02-07 09:37:51 +0700

    feat: add flutter_native_splash.yaml and splash screen -

--- lib/main.dart
+++ lib/main.dart
@@ -2,111 +2,119 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:music_app/app.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'core/config/firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  //await addMultipleSongs();
+  
+  // Remove splash screen
+  FlutterNativeSplash.remove();
+  
+  // await addMultipleSongs();
   runApp(const MusicApp());
 }
-  Future<void> addMultipleSongs() async {
-    FirebaseFirestore db = FirebaseFirestore.instance;
-    WriteBatch batch = db.batch();
 
-    List<Map<String, dynamic>> songs = [
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
+Future<void> addMultipleSongs() async {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  WriteBatch batch = db.batch();
 
-      },
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
-      },
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
-      },
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
-      },
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
-      },
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
-      },
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
-      },
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
-      },
-      {
-        'title': '',
-        'artistId': '',
-        'albumId': 'xyz',
-        'duration': 180,
-        'image': '',
-        'source': '',
-        'is_favorite': ''
-      },
-    ];
+  List<Map<String, dynamic>> songs = [
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+    {
+      'title': '',
+      'artistId': '',
+      'albumId': 'xyz',
+      'duration': 180,
+      'image': '',
+      'source': '',
+      'is_favorite': ''
+    },
+  ];
 
-    for (var song in songs) {
-      DocumentReference docRef = db.collection('songs').doc();
-      batch.set(docRef, song);
-    }
-
-    await batch.commit();
+  for (var song in songs) {
+    DocumentReference docRef = db.collection('songs').doc();
+    batch.set(docRef, song);
   }
+
+  await batch.commit();
+}
+
+class SeedDataScreen extends StatelessWidget {
+  const SeedDataScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -115,12 +123,15 @@ void main() async{
         child: ElevatedButton(
           onPressed: () async {
             await addMultipleSongs();
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Đã thêm bài hát')),
-            );
+            if (context.mounted) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Đã thêm bài hát')),
+              );
+            }
           },
-          child: Text('Thêm nhiều bài hát'),
+          child: const Text('Thêm nhiều bài hát'),
         ),
       ),
     );
   }
+}
