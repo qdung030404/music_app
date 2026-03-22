836928befd25395a7fe804ac2f21dc363ab5864e
qdung030404 <qdung030404@gmail.com>
2026-03-22 22:35:14 +0700

    Chore: Clean Architecture of Old Firebase - Completely remove 9 repository files and use cases related to loading internal music/artists/albums (SongRepository, GetSongs, etc.).
    - Update imports throughout the project so the app runs independently and doesn't depend on Firestore.

--- lib/domain/repositories/artist_repository.dart
+++
@@ -1,5 +0,0 @@
-import '../entities/artist_entity.dart';
-
-abstract interface class ArtistRepository {
-  Future<List<Artist>> getArtists();
-}
--- lib/domain/repositories/song_repository.dart
+++
@@ -1,5 +0,0 @@
-import '../entities/song_entity.dart';
-
-abstract interface class SongRepository {
-  Future<List<Song>?> getSongs();
-}
