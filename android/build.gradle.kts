d7aa3aaf03e3c32e2e064e57eb5a1992fb86e087
qdung030404 <qdung030404@gmail.com>
2026-04-02 13:33:28 +0700

    build(android): force Java 17 and Kotlin JVM target 17 for all subprojects to fix obsolete Java 8 warnings

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,20 @@
+subprojects {
+    afterEvaluate {
+        if (project.extensions.findByName("android") != null) {
+            @Suppress("UNCHECKED_CAST")
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.compileOptions.sourceCompatibility = JavaVersion.VERSION_17
+            android.compileOptions.targetCompatibility = JavaVersion.VERSION_17
+            
+            // Nếu plugin có sử dụng Kotlin, cũng sẽ thiết lập jvmTarget về 17
+            if (project.plugins.hasPlugin("org.jetbrains.kotlin.android")) {
+                val kotlinOptions = project.extensions.getByName("kotlinOptions") as org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+                kotlinOptions.jvmTarget = "17"
+            }
+        }
+    }
+}
+
 allprojects {
     repositories {
         google()
